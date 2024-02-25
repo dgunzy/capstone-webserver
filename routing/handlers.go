5f738a0b4995c02d8d646f7db70c6e490406803f
dgunzy <danbguns@gmail.com>
2024-02-25 14:24:20 -0400

    added tailwind

--- routing/handlers.go
+++ routing/handlers.go
@@ -11,10 +11,10 @@ import (
 // HomeHandler handles requests to the home page.
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("routing/templates/index.gohtml"))
-	data := map[string]interface{}{
-		"Title": "HTMX Example",
-	}
-	if err := tmpl.Execute(w, data); err != nil {
+	// data := map[string]interface{}{
+	// 	"Title": "HTMX Example",
+	// }
+	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
